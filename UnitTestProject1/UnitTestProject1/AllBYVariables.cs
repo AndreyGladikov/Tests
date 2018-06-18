18393637d7ecde1d7e6b9702cf2d2183886c3f04
AndreyGladikov <andreygladikov@coherentsolutions.com>
2018-06-18 17:51:27 +0300

    Task20

---
+++ UnitTestProject1/UnitTestProject1/AllBYVariables.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+
+
+//namespace AllByVariables
+//{
+//    public class By
+//    {
+//        private static IWebDriver driver;
+//        driver.FindElement(By.Id("Username"));
+//        driver.FindElement(By.Xpath("//input[@id='Password']"));
+//        driver.FindElement(By.ClassName("submit-button"));
+//        driver.FindElement(By.TagName("button"));
+//        driver.FindElement(By.Name("Username"));
+//        driver.FindElement(By.LinkText("Use single sign-on"));
+//        driver.FindElement(By.CssSelector("input[id=Username]"));
+//    }
+//}
---
+++ UnitTestProject1/UnitTestProject1/Task_20.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class Task_20
+    {
+        
+        private static String URL = "https://192.168.100.26/";
+        private static IWebDriver driver;
+        private string ExpectedURL = "https://192.168.100.26/Home/Index";
+        public void Login(string username, string password)
+        {
+           driver.Navigate().GoToUrl(URL);
+           driver.FindElement(By.CssSelector("input[id=Username]")).SendKeys(username);
+           driver.FindElement(By.Id("Password")).SendKeys(password);
+          // driver.FindElement(By.Id("SubmitButton")).Click();
+            driver.FindElement(By.TagName("button")).Click();
+        }
+
+        [TestInitialize]
+        public void initialize()
+        {
+           driver = new ChromeDriver();
+    }
+        [TestMethod]
+        [TestCategory("Chrome")]
+        public void Task20_Test1()
+            
+        {
+            Login("EugenBorisik", "qwerty12345");
+            string CurrentURL = driver.Url;
+            Assert.AreEqual(ExpectedURL, CurrentURL);
+            
+
+        }
+
+        [TestMethod]
+        [TestCategory("Chrome")]
+        public void Task20_Test2()
+        {
+            Login("EugenBorisik", "qwerty12345");
+            driver.FindElement(By.ClassName("sign-out")).Click();
+            string CurrentURL = driver.Url;
+            Assert.AreEqual(URL, CurrentURL);
+            
+        }
+    [TestCleanup]
+    public void CleanUp()
+    {
+        driver.Quit();
+    }
+    }
+}
+// static IWebDriver driver = new ChromeDriver();
